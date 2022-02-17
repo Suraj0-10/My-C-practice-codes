71ad1a49fe56c9b30485dcf14ae56931738f3889
Suraj <98628922+Suraj0-10@users.noreply.github.com>
2022-02-17 19:07:07 +0530

    Add files via upload

---
+++ basic_calc_using_ifelse.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,op;
+    printf("Enter the first number :");
+    scanf("%d",&a);
+    printf("Enter the second number :");
+    scanf("%d",&b);
+    printf("Enter the operator : ");
+    scanf(" %c",&op);
+    
+    if( op == '+')
+    {
+        printf("%d + %d = %d",a,b,a+b);
+    }
+    else if( op == '-')
+    {
+        printf("%d - %d = %d",a,b,a-b);
+    }
+    else if( op == '*')
+    {
+        printf("%d x %d = %d",a,b,a*b);
+    }
+    else if( op == '/')
+    {
+        printf("%d / %d = %d",a,b,a/b);
+    }
+    else 
+    {
+        printf("Invalid");
+    }
+}
