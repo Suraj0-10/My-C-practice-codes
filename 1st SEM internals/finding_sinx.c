c5a2176cefd70de63198d56cd1309680f5a86398
Suraj <98628922+Suraj0-10@users.noreply.github.com>
2022-03-25 12:35:14 +0530

    Update finding_sinx.c

--- 1st SEM internals/finding_sinx.c
+++ 1st SEM internals/finding_sinx.c
@@ -9,10 +9,11 @@ int  main()
 	scanf("%d",&x);
 	x=x*(3.1415/180);
 	t=x;
-           sum=x;
+        sum=x;
  	for(i=1; i<=n; i++)
-	{    t=(t*(-1)*x*x ) / (2*i*(2*i+1));
-      		sum=sum+t;
+	{    
+           t=(t*(-1)*x*x)/(2*i*(2*i+1));
+      	   sum=sum+t;
 	}
 	printf(“The  value of sine(%f)=%f\n”,x,sum);
 	printf(“Using library function=%f”, sin(x));
