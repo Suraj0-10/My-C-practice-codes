22054ab853b7ab0ebc3b1ca06e63d8c2288c9e8f
Suraj <98628922+Suraj0-10@users.noreply.github.com>
2022-04-09 19:09:47 +0530

    Add files via upload

---
+++ 1st SEM internals/string_functions.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+#include<string.h>
+void concat(char[],char[]);
+void len(char[]);
+void compare(char[],char[]);
+void main()
+{
+  char s1[20],s2[20];
+  printf("\nEnter the string 1 : ");
+  gets(s1);
+  len(s1);
+  printf("\nEnter the string 2 : ");
+  gets(s2);
+  len(s2);
+  concat(s1,s2);
+  printf("The concatenated string is %s\n",s1);
+  compare(s1,s2);
+}
+void concat(char s1[],char s2[])
+{
+  int i,j;
+  i=strlen(s1);
+  for(j=0;s2[j]!='\0';i++,j++)
+  {
+    s1[i]=s2[j];
+  }
+  s1[i]='\0';
+}
+void len(char s1[])
+{
+  int i,length=0;
+  for(i=0;s1[i]!='\0';i++)
+  {
+    length++;
+  }
+  printf("The length of the string is %d\n",length);
+}
+void compare(char s1[],char s2[])
+{
+  int i;
+  for(i=0;s1[i]==s2[i] && s1[i]=='\0';i++);
+  if(s1[i]>s2[i] && s1[i]<s2[i])
+  {
+    printf("Both strings are not equal\n");
+  }
+  else if(s1[i]==s2[i])
+  {
+    printf("Both strings are equal\n");
+  }
+}
