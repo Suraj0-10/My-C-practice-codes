a7591b7199f66467559dc75c2bb2f77e87b157ff
Suraj <98628922+Suraj0-10@users.noreply.github.com>
2022-04-11 18:55:37 +0530

    Add files via upload

---
+++ 1st SEM internals/pointer_mean_sum_SD.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+    float a[10],*ptr,mean,std,sum=0,sumstd=0;
+    int n,i;
+    printf("Enter the number of elements : ");
+    scanf("%d",&n);
+    printf("\nEnter the array elements : ");
+    for(i=0;i<n;i++)
+    {
+        scanf("%f",&a[i]);
+    }
+    ptr = a;
+    for(i=0;i<n;i++)
+    {
+        sum = sum + *ptr;
+        ptr++;
+    }
+    mean = sum/n;
+    ptr=a;
+    for(i=0;i<n;i++)
+    {
+        sumstd = sumstd+pow((*ptr-mean),2);
+        ptr++;
+    }
+    std = sqrt(sumstd/n);
+    printf("\nSum = %f\t",sum);
+    printf("\nMean = %f\t",mean);
+    printf("\nStandard deviation = %f\t",std);
+}
