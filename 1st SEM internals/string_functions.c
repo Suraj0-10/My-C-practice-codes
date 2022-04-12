e524e8f2961528ce90795a677e839e68d47876f5
Suraj <98628922+Suraj0-10@users.noreply.github.com>
2022-04-12 10:25:05 +0530

    Update string_functions.c

--- 1st SEM internals/string_functions.c
+++ 1st SEM internals/string_functions.c
@@ -39,7 +39,7 @@ void compare(char s1[],char s2[])
 {
   int i;
   for(i=0;s1[i]==s2[i] && s1[i]=='\0';i++);
-  if(s1[i]>s2[i] && s1[i]<s2[i])
+  if(s1[i]>s2[i] || s1[i]<s2[i])
   {
     printf("Both strings are not equal\n");
   }
